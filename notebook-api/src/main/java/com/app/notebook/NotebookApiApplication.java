9e69d84f10ca10a37fdaa7aa055d6148eec565cc
umang8496 <umang8april@gmail.com>
2021-01-31 11:09:42 +0530

    Initial commit for NotebookApiApplication

---
+++ notebook-api/src/main/java/com/app/notebook/NotebookApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.notebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookApiApplication.class, args);
+	}
+
+}
---
+++ notebook-api/src/test/java/com/app/notebook/NotebookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
