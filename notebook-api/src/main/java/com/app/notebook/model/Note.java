b87cbe7a60edff2c533d1b5fbc8278fc97dfd060
umang8496 <umang8april@gmail.com>
2021-01-31 13:50:33 +0530

    Models for the NotebookApi app

---
+++ notebook-api/src/main/java/com/app/notebook/model/Note.java
@@ -0,0 +1,85 @@
+package com.app.notebook.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity(name = "Note")
+public class Note {
+	@Id
+	@Column(name = "noteId", nullable = false)
+	private UUID noteId;
+
+	@Column(name = "noteName", nullable = false, length = 64)
+	private String noteName;
+
+	@Column(name = "noteContent", nullable = false, length = 256)
+	private String noteContent;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Notebook notebook;
+
+	@Column(name = "lastModifiedOn")
+	private Date lastModifiedOn;
+
+	public Note() {
+		this.noteId = UUID.randomUUID();
+		this.lastModifiedOn = new Date();
+	}
+
+	public Note(String noteName, String noteContent, Notebook notebook) {
+		this.noteId = UUID.randomUUID();
+		this.lastModifiedOn = new Date();
+		this.noteName = noteName;
+		this.notebook = notebook;
+		this.noteContent = noteContent;
+	}
+
+	public Note(String noteId, String noteName, String noteContent, Notebook notebook) {
+		this(noteName, noteContent, notebook);
+		if (noteId != null) {
+			this.noteId = UUID.fromString(noteId);
+		}
+	}
+
+	public UUID getNoteId() {
+		return this.noteId;
+	}
+
+	public String getNoteName() {
+		return this.noteName;
+	}
+
+	public void setNoteName(String noteName) {
+		this.noteName = noteName;
+	}
+
+	public String getNoteContent() {
+		return this.noteContent;
+	}
+
+	public void setNoteContent(String noteContent) {
+		this.noteContent = noteContent;
+	}
+
+	public Notebook getNotebook() {
+		return this.notebook;
+	}
+
+	public String getNotebookId() {
+		return this.notebook.getNotebookId().toString();
+	}
+
+	public Date getLastModifiedOn() {
+		return this.lastModifiedOn;
+	}
+
+	public void setLastModifiedOn(Date lastModifiedOn) {
+		this.lastModifiedOn = lastModifiedOn;
+	}
+}
---
+++ notebook-api/src/main/java/com/app/notebook/model/Notebook.java
@@ -0,0 +1,67 @@
+package com.app.notebook.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity(name = "Notebook")
+public class Notebook {
+	@Id
+	@Column(name = "notebookId", nullable = false)
+	private UUID notebookId;
+
+	@Column(name = "notebookName", nullable = false, length = 64)
+	private String notebookName;
+
+	@Column(name = "notes")
+	@OneToMany(mappedBy = "notebook", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JsonIgnore
+	private List<Note> notes;
+
+	public Notebook() {
+		this.notebookId = UUID.randomUUID();
+		this.notes = new ArrayList<>();
+	}
+
+	public Notebook(String notebookName) {
+		this();
+		this.notebookName = notebookName;
+	}
+
+	public Notebook(UUID notebookId, String notebookName) {
+		this();
+		if (notebookId != null) {
+			this.notebookId = UUID.randomUUID();
+		}
+		this.notebookName = notebookName;
+	}
+
+	public UUID getNotebookId() {
+		return this.notebookId;
+	}
+
+	public String getNotebookName() {
+		return this.notebookName;
+	}
+
+	public void setNotebookName(String notebookName) {
+		this.notebookName = notebookName;
+	}
+
+	public List<Note> getNotes() {
+		return this.notes;
+	}
+
+	public int getNumberOfNotes() {
+		return this.notes.size();
+	}
+}
