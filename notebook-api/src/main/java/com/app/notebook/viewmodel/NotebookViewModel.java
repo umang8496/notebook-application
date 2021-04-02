ae5b0cb2e8dbd805de39ed41a6d71464ff983070
umang8496 <umang8april@gmail.com>
2021-04-02 17:53:18 +0530

    Create View models for the REST end-points

---
+++ notebook-api/src/main/java/com/app/notebook/viewmodel/NotebookViewModel.java
@@ -0,0 +1,37 @@
+package com.app.notebook.viewmodel;
+
+import javax.validation.constraints.NotNull;
+
+public class NotebookViewModel {
+	@NotNull
+	private String id;
+
+	@NotNull
+	private String name;
+
+	private int nbNotes;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNbNotes() {
+		return nbNotes;
+	}
+
+	public void setNbNotes(int nbNotes) {
+		this.nbNotes = nbNotes;
+	}
+}
