85f96aca46560ca2672c95b2765ca5cee6a2ab8b
umang8496 <umang8april@gmail.com>
2021-04-02 17:32:03 +0530

    Remove the irrelevant functionality from the Application

--- notebook-api/src/main/java/com/app/notebook/NotebookApiApplication.java
+++ notebook-api/src/main/java/com/app/notebook/NotebookApiApplication.java
@@ -2,19 +2,10 @@ package com.app.notebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class NotebookApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NotebookApiApplication.class, args);
-	}
-
-	@RequestMapping("/hello")
-	public String helloNotebookApiApplication() {
-		return "Hello NotebookApi Application";
 	}
 }
